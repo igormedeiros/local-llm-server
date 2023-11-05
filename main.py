a4db0d10fef51a3ff4b5e085ec7d0296639b1696
Igor Medeiros <igor.medeiros@gmail.com>
2023-11-05 00:59:27 -0300

    init model endpoint added

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-import logging
 from fastapi import FastAPI, APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from pydantic import BaseModel
+import logging
+
+app = FastAPI()
+router = APIRouter(prefix="/v1")
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
-router = APIRouter(prefix="/v1")
+# Function to map the short name to the actual model name
+def map_model_name(model_name_short: str) -> str:
+    model_name_map = {
+        "mistral": "mistralai/Mistral-7B-v0.1"
+    }
+    return model_name_map.get(model_name_short.lower(), model_name_short)
 
-# Initialize variables to None
-tokenizer = None
+# Global variables for model and tokenizer
 model = None
+tokenizer = None
 
 class GenerateRequest(BaseModel):
     prompts: list
@@ -24,32 +31,49 @@ class GenerateRequest(BaseModel):
     presence_penalty: float
     stop: list
 
+# New endpoint to initialize the model
+@router.get("/init_model/{model_name_short}")
+async def init_model(model_name_short: str):
+    global model, tokenizer
+
+    if model is None or tokenizer is None:
+        model_name = map_model_name(model_name_short)
+        try:
+            logger.info(f"Initializing model: {model_name}")
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            model = AutoModelForCausalLM.from_pretrained(model_name)
+            logger.info("Model initialized successfully.")
+            return {"message": f"Model {model_name} loaded successfully"}
+        except Exception as e:
+            logger.error(f"Error loading the model: {e}")
+            raise HTTPException(status_code=500, detail="Model could not be loaded")
+    else:
+        return {"message": "Model already initialized"}
+
+# The generate text endpoint now assumes the model is already loaded
 @router.post("/{model_name_short}/completions")
 async def generate_text(model_name_short: str, request: GenerateRequest):
-    if tokenizer is None or model is None:
-        logger.error("Model or tokenizer not loaded.")
-        raise HTTPException(status_code=503, detail="Model is not loaded yet.")
-    
-    logger.info("Generating text...")
+    if model is None or tokenizer is None:
+        raise HTTPException(status_code=500, detail="Model is not initialized")
+
     inputs = tokenizer(request.prompts[0], return_tensors="pt", truncation=True)
     outputs = model.generate(**inputs, max_length=request.max_tokens,
                              temperature=request.temperature,
-                             top_p=request.top_p)
-    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    logger.info("Text generated.")
-    return {"generated_text": generated_text}
+                             top_p=request.top_p,
+                             frequency_penalty=request.frequency_penalty,
+                             presence_penalty=request.presence_penalty,
+                             stop_words=request.stop)
+    
+    response_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    logger.info("Generated text successfully.")
+    return {"generated_text": response_text}
 
 @router.get("/health")
 def health_check():
-    return JSONResponse(content={"status": "Model is loaded and service is healthy." if model and tokenizer else "Model is not loaded yet."})
-
-@app.on_event("startup")
-async def load_model():
-    global tokenizer, model
-    logger.info("Loading model, this might take a while...")
-    model_name = "mistralai/Mistral-7B-v0.1"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name)
-    logger.info("Model loaded.")
+    return JSONResponse(content={"status": "ok"})
 
 app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=1234)
