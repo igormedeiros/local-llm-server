9d5735c5c86069f929fbe0298b371e36b5b7aed3
Igor Medeiros <igor.medeiros@gmail.com>
2023-11-05 00:33:11 -0300

    Model and Tokenizer are loaded once when the server starts

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from pydantic import BaseModel
 app = FastAPI()
 router = APIRouter(prefix="/v1")
 
+# Model and Tokenizer are loaded once when the server starts
+model_name = "mistralai/Mistral-7B-v0.1"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
 class GenerateRequest(BaseModel):
     prompts: list
     max_tokens: int
@@ -15,20 +20,8 @@ class GenerateRequest(BaseModel):
     presence_penalty: float
     stop: list
 
-def map_model_name(model_name_short: str) -> str:
-    return {
-        "mistral": "mistralai/Mistral-7B-v0.1"
-    }.get(model_name_short.lower(), model_name_short)
-
-def load_model(model_name: str):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name)
-    return tokenizer, model
-
 @router.post("/{model_name_short}/completions")
 async def generate_text(model_name_short: str, request: GenerateRequest):
-    model_name = map_model_name(model_name_short)
-    tokenizer, model = load_model(model_name)
     inputs = tokenizer(request.prompts[0], return_tensors="pt", truncation=True)
     outputs = model.generate(**inputs, max_length=request.max_tokens,
                              temperature=request.temperature,
